87c411583d7c2231b2037ccc0109d84464b449fe
kjac <kja@umbraco.dk>
2023-06-23 09:06:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Umbraco Headless Blog Sample
+
+TODO TODO
+
+```bash
+dotnet add package Umbraco.Sample.Headless.Blog
+```
+
+TODO TODO
---
+++ src/Umbraco.Sample.Headless.Blog/Migrations/SampleBlogMigrationPlan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Umbraco.Cms.Core.Packaging;
+
+namespace Umbraco.Sample.Headless.Blog.Migrations;
+
+public class SampleBlogMigrationPlan : PackageMigrationPlan
+{
+    public SampleBlogMigrationPlan() : base("Blog Sample Data")
+    {
+    }
+
+    protected override void DefinePlan()
+        => To<InstallSampleDataPackageMigration>(new Guid("D18BE3B5-B247-47DE-8FFE-8D9D6522F415"));
+}
